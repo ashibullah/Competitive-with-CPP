751674eee091790ffa8ec891a0d25ad9064378cf
Ashib Ullah <ashibullahbhai@gmail.com>
2023-08-14 01:57:32 +0600

    3rd

--- Codeforces/sumofodd.cpp
+++ Codeforces/sumofodd.cpp
@@ -1,30 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-int T;
+typedef long long ll;
 int main(){
-	cin>>T;
-	while(T--){
-		int n;
-        cin>>n;
-         vector<int> a;
-
-         int num;
-        while (cin >> num) {
-        a.push_back(num);
-          }
-
-        sort(a.begin(), a.end());
-
-    cout << "Sorted array: ";
-    for (int num : a) {
-        cout << num << " ";
-    } 
+	ll x;
+    cin>>x;
+    ll sum = 0,odd = 1;
+    for (auto i = 0; i < x; i++)
+    {
+        sum+= odd;
+        odd+= 2;
         
-        
-        
-
-	}
-
+    }
+    cout<<sum;
+    
 
 	return 0;
 }
